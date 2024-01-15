8ec58de48a3ef0b24c996145e3262462a5ac46d2
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-15 11:37:18 +0530

    added : landing page added

---
+++ components/logo.tsx
@@ -0,0 +1,9 @@
+import { WorkflowIcon } from "lucide-react";
+
+const Logo = () => {
+    return (  
+        <div className=""> <WorkflowIcon className="h-6 w-6" /></div>
+    );
+}
+ 
+export default Logo;
