c3e6a03444d9a7c2de4e96b896f43d67a02723fc
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-26 19:52:08 +0530

    added : unsplash added now with board title image will also be added

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,25 +3,30 @@ import { Button } from '@/components/ui/button'
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
 import { MobileSidebar } from './mobile-sidebar'
+import { Popover } from '@/components/ui/popover'
+import { FormPopover } from '@/components/form/form-popover'
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4  w-full h-14 border-b shadow-sm bg-white flex items-center">
-        <MobileSidebar/>
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-
-        <Button
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1 px-2"
-        >
-          Create
-        </Button>
-        <Button size="sm" className="rounded-sm block md:hidden">
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            size="sm"
+            className="rounded-sm hidden md:block h-auto py-1 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button size="sm" className="rounded-sm block md:hidden">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
@@ -32,9 +37,9 @@ const Navbar = () => {
           appearance={{
             elements: {
               rootBox: {
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               },
             },
           }}
@@ -46,8 +51,8 @@ const Navbar = () => {
               avatarBox: {
                 height: 30,
                 width: 30,
-              }
-            }
+              },
+            },
           }}
         />
       </div>
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,8 @@ import { X } from 'lucide-react'
 import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
+import { FormPicker } from './form-picker'
+import { ElementRef, useRef } from 'react'
 
 interface FormPopoverProps {
   children: React.ReactNode
@@ -29,9 +31,11 @@ export const FormPopover = ({
   sideOffset = 0,
 }: FormPopoverProps) => {
   const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
       toast.success("Board created!");
+      closeRef.current?.click();
      
       router.push(`/board/${data.id}`);
     },
@@ -42,9 +46,9 @@ export const FormPopover = ({
   });
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
-  
+    const image = formData.get("image") as string;
 
-    execute({ title });
+    execute({ title, image });
   }
   return (
     <Popover>
@@ -58,7 +62,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600' variant="ghost">
           <X className='h-4 w-4'/>
 
@@ -67,7 +71,10 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
-          
+          <FormPicker
+              id="image"
+              errors={fieldErrors}
+            />
             <FormInput
               id="title"
               label="Board title"
