af640c28b54fadd967d68a330d5efd036b76f196
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-26 19:58:54 +0530

    fix : now all created board wiil be displayed

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from '@/components/ui/separator'
 import { Info } from './_components/info'
 import BoardList from './_components/board-list'
+import { Suspense } from 'react'
 
 const OrganizationIdPage = () => {
   return (
@@ -8,7 +9,10 @@ const OrganizationIdPage = () => {
       <Info />
       <Separator className="my-4" />
       <div className='px-2 md:px-4'>
+        <Suspense fallback={<BoardList.Skeleton/>}>
         <BoardList/>
+
+        </Suspense>
       </div>
     </div>
   )
