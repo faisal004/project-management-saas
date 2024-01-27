ff96e1d93005c50c4fed70569aaa5080b1dd978d
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-27 20:54:47 +0530

    added : list can be added ,updated ,deleted and can be copied

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,18 +1,28 @@
-"use client"
+'use client'
 
-import { ListWithCards } from "@/types";
-import ListForm from "./list-form";
+import { ListWithCards } from '@/types'
+import ListForm from './list-form'
+import { useEffect, useState } from 'react'
+import ListItem from './list-item'
 
-interface ListContainerProps{
-    data:ListWithCards[];
-    boardId:string;
+interface ListContainerProps {
+  data: ListWithCards[]
+  boardId: string
 }
 
-export const ListContainer=({data,boardId}:ListContainerProps)=>{
-    return (
-        <ol>
-            <ListForm/>
-            <div className="flex-shrink-0 w-1"/>
-        </ol>
-    )
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data)
+
+  useEffect(() => {
+    setOrderedData(data)
+  }, [data])
+  return (
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />
+      })}
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  )
 }
