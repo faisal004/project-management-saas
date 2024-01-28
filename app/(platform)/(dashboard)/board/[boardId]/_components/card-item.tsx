646c00c0d9d388ef00fa311e29f3e7ba2df02ba7
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-28 14:20:19 +0530

    fix : css fix

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,18 +1,16 @@
-"use client"
+'use client'
 
-import { Card } from "@prisma/client"
+import { Card } from '@prisma/client'
 
-interface CardItemProps{
-    data:Card,
-    index:number;
+interface CardItemProps {
+  data: Card
+  index: number
 }
 
-export const CardItem=({data,index}:CardItemProps)=>{
-    return (
-        <div           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-        >
-            {data.title}
-        </div>
-    )
-
+export const CardItem = ({ data, index }: CardItemProps) => {
+  return (
+    <div role='button' className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+      {data.title}
+    </div>
+  )
 }
